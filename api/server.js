4717d965b639e68474177533fa020cc8b7c3094f
AnhPT <tuananh200898@gmail.com>
2018-03-03 13:59:43 +0700

    Link NodeJs Container to run same network with mongoDB service

--- api/server.js
+++ api/server.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const app = express();
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://mongodb/ustory');
+
 // Constants
 const PORT = 6969;
 const HOST = '0.0.0.0';
+const MONGO_HOST = 'localhost';
+const MONGO_PORT = 27017;
 
-app.get('/', (req, res) => res.send('UStory is running......'));
-require('./User/UserController')(app);
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('Connect to mongoDB success');
+  app.get('/', (req, res) => res.send('UStory is runningggg......'));
+  require('./User/UserController')(app);
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+  app.listen(PORT, HOST);
+  console.log(`Running on http://${HOST}:${PORT}`);
+});

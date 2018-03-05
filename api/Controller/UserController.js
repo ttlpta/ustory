5e653854dfa0167666165df19207670bea9174ef
AnhPT <tuananh200898@gmail.com>
2018-03-05 23:38:20 +0700

    User Login function

--- api/Controller/UserController.js
+++ api/Controller/UserController.js
@@ -2,18 +2,37 @@ const _ = require('lodash');
 
 module.exports = (app, mongoose) => {
   const userModel = require('../Model/UserModel')(mongoose);
+
   app.post('/user/regist', (req, res) => {
     const base64Pw = _.isUndefined(req.body.password) ? '' : new Buffer(req.body.password).toString('base64');
     const user = new userModel({...req.body, password : base64Pw });
     user.save((err) => {
       if(err){
-        if(err.message.indexOf('duplicate key error') !== -1) {
-          return res.json({success: false, message: 'Email is existed'});
-        }
-        
-        return res.json({success: false, message: err.message});
+        return res.json({success: false, message: (err.code == 11000) ? 'Email is existed' : err.message});
       }
       return res.json({success: true});
     });
   });
+
+  app.post('/user/login', (req, res) => {
+    const base64Pw = _.isUndefined(req.body.password) ? '' : new Buffer(req.body.password).toString('base64');
+    const email = _.isUndefined(req.body.password) ? '' : req.body.email;
+    if(!email && !base64Pw){
+      return res.json({});
+    }
+    userModel.findOne({ email, password: base64Pw }, (err, user) => {
+      if(err) return res.status(500).end();
+      
+      if(_.isEmpty(user))
+        return res.json({ success: false, message: 'Email or password is invalid' });
+
+      return res.json({
+        success: true,
+        user : {
+          id: user._id,
+          email: user.email
+        }
+      });
+    });
+  })
 }
